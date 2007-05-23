ca6940e60862e93a524195a135cee37507b778ad
wizardspro <wizardspro@4e566d00-7630-0410-a8d3-e317d81b8117>
2007-05-23 11:27:44 +0000

    Added ATOM 1.0 write support! Should work perfectly now. Fixed a few bugs. Added the main functions for uploading via FTP. Still cannot handle exceptions very well. Overall, Work progressing well! Beta to be released in 2 weeks.

--- Connect.Designer.cs
+++ Connect.Designer.cs
@@ -31,20 +31,22 @@ namespace FeedCreator.NET
             this.components = new System.ComponentModel.Container();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.passwordTextBox = new System.Windows.Forms.MaskedTextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.serverTextBox = new System.Windows.Forms.TextBox();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
             this.fileName = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.uploadButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
             this.directoryPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,9 +59,66 @@ namespace FeedCreator.NET
             this.toolTip1.AutoPopDelay = 10000;
             this.toolTip1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.toolTip1.InitialDelay = 500;
-            this.toolTip1.ReshowDelay = 75;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
             this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.toolTip1.ToolTipTitle = "FeedLaunch Interactive Help";
+            this.toolTip1.ToolTipTitle = "Feed Launch Interactive Help";
+            // 
+            // serverTextBox
+            // 
+            this.serverTextBox.Location = new System.Drawing.Point(109, 27);
+            this.serverTextBox.Name = "serverTextBox";
+            this.serverTextBox.Size = new System.Drawing.Size(136, 20);
+            this.serverTextBox.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.serverTextBox, "Enter the name/IP address or the FTP server.");
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(109, 54);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(136, 20);
+            this.usernameTextBox.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.usernameTextBox, "Enter your username here.");
+            // 
+            // fileName
+            // 
+            this.fileName.Enabled = false;
+            this.fileName.Location = new System.Drawing.Point(15, 144);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(292, 20);
+            this.fileName.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.fileName, "Enter the desired upload filename. Leave empty and the feed will retain its origi" +
+                    "nal name.");
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(79, 301);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 8;
+            this.uploadButton.Text = "Upload";
+            this.toolTip1.SetToolTip(this.uploadButton, "Uploads feed to FTP server");
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(160, 301);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.toolTip1.SetToolTip(this.cancelButton, "Cancels upload and closes this window");
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // directoryPath
+            // 
+            this.directoryPath.Location = new System.Drawing.Point(15, 211);
+            this.directoryPath.Name = "directoryPath";
+            this.directoryPath.Size = new System.Drawing.Size(295, 20);
+            this.directoryPath.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.directoryPath, "Enter the directory path");
             // 
             // label1
             // 
@@ -79,17 +138,6 @@ namespace FeedCreator.NET
             this.label2.TabIndex = 1;
             this.label2.Text = "Username";
             // 
-            // passwordTextBox
-            // 
-            this.passwordTextBox.Location = new System.Drawing.Point(109, 83);
-            this.passwordTextBox.Mask = "        ";
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.PasswordChar = '*';
-            this.passwordTextBox.Size = new System.Drawing.Size(136, 20);
-            this.passwordTextBox.TabIndex = 2;
-            this.toolTip1.SetToolTip(this.passwordTextBox, "Enter your password here.\r\n");
-            this.passwordTextBox.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -98,31 +146,6 @@ namespace FeedCreator.NET
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Password";
-            // 
-            // serverTextBox
-            // 
-            this.serverTextBox.Location = new System.Drawing.Point(109, 27);
-            this.serverTextBox.Name = "serverTextBox";
-            this.serverTextBox.Size = new System.Drawing.Size(201, 20);
-            this.serverTextBox.TabIndex = 4;
-            this.toolTip1.SetToolTip(this.serverTextBox, "Enter the name/IP address or the FTP server.");
-            // 
-            // usernameTextBox
-            // 
-            this.usernameTextBox.Location = new System.Drawing.Point(109, 54);
-            this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(201, 20);
-            this.usernameTextBox.TabIndex = 5;
-            this.toolTip1.SetToolTip(this.usernameTextBox, "Enter your username here.");
-            // 
-            // fileName
-            // 
-            this.fileName.Location = new System.Drawing.Point(15, 144);
-            this.fileName.Name = "fileName";
-            this.fileName.Size = new System.Drawing.Size(292, 20);
-            this.fileName.TabIndex = 6;
-            this.toolTip1.SetToolTip(this.fileName, "Enter the desired upload filename. Leave empty and the feed will retain its origi" +
-                    "nal name.");
             // 
             // label4
             // 
@@ -133,27 +156,6 @@ namespace FeedCreator.NET
             this.label4.TabIndex = 7;
             this.label4.Text = "Directory Path(relative)";
             // 
-            // uploadButton
-            // 
-            this.uploadButton.Location = new System.Drawing.Point(79, 301);
-            this.uploadButton.Name = "uploadButton";
-            this.uploadButton.Size = new System.Drawing.Size(75, 23);
-            this.uploadButton.TabIndex = 8;
-            this.uploadButton.Text = "Upload";
-            this.toolTip1.SetToolTip(this.uploadButton, "Uploads feed to FTP server");
-            this.uploadButton.UseVisualStyleBackColor = true;
-            // 
-            // cancelButton
-            // 
-            this.cancelButton.Location = new System.Drawing.Point(160, 301);
-            this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 9;
-            this.cancelButton.Text = "Cancel";
-            this.toolTip1.SetToolTip(this.cancelButton, "Cancels upload and closes this window");
-            this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.button2_Click);
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -162,14 +164,6 @@ namespace FeedCreator.NET
             this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 10;
             this.label5.Text = "FileName";
-            // 
-            // directoryPath
-            // 
-            this.directoryPath.Location = new System.Drawing.Point(15, 211);
-            this.directoryPath.Name = "directoryPath";
-            this.directoryPath.Size = new System.Drawing.Size(295, 20);
-            this.directoryPath.TabIndex = 11;
-            this.toolTip1.SetToolTip(this.directoryPath, "Enter the directory path");
             // 
             // progressBar1
             // 
@@ -187,12 +181,40 @@ namespace FeedCreator.NET
             this.label6.TabIndex = 13;
             this.label6.Text = "Upload Progress";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(251, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Port";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(284, 26);
+            this.portTextBox.MaxLength = 12;
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(46, 20);
+            this.portTextBox.TabIndex = 15;
+            this.portTextBox.Text = "21";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(109, 86);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(136, 20);
+            this.passwordTextBox.TabIndex = 16;
+            // 
             // UploadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(342, 336);
             this.ControlBox = false;
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.directoryPath);
@@ -204,7 +226,6 @@ namespace FeedCreator.NET
             this.Controls.Add(this.usernameTextBox);
             this.Controls.Add(this.serverTextBox);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
@@ -231,12 +252,14 @@ namespace FeedCreator.NET
         private System.Windows.Forms.Button cancelButton;
         public System.Windows.Forms.TextBox usernameTextBox;
         public System.Windows.Forms.TextBox serverTextBox;
-        public System.Windows.Forms.MaskedTextBox passwordTextBox;
         public System.Windows.Forms.TextBox fileName;
         public System.Windows.Forms.Button uploadButton;
         private System.Windows.Forms.TextBox directoryPath;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox passwordTextBox;
     }
 }
--- Connect.cs
+++ Connect.cs
@@ -13,6 +13,8 @@ namespace FeedCreator.NET
 {
     public partial class UploadForm : Form
     {
+        public string strFileName = "";
+        int uploadTry = 1;
         FTPConnect UploadConnection;
         public UploadForm()
         {
@@ -32,6 +34,90 @@ namespace FeedCreator.NET
         private void UploadForm_Load(object sender, EventArgs e)
         {
             UploadConnection = new FTPConnect();
+        }
+
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            bool run = true;
+            if(serverTextBox.Text == "")
+            {
+                errorProvider1.SetError(serverTextBox, "Server name cannot be empty!");
+                run = false;
+            }
+            if(usernameTextBox.Text == "")
+            {
+                errorProvider1.SetError(usernameTextBox, "Username cannot be empty!");
+            run = false;
+            }
+            if(passwordTextBox.Text == "")
+            {
+                errorProvider1.SetError(passwordTextBox, "Password cannot be empty!");
+                run = false;
+            }
+            try
+            {
+                int i = Convert.ToInt32(portTextBox.Text);
+            }
+            catch(Exception ex)
+            {
+                errorProvider1.SetError(portTextBox, "Port number is invalid!");
+                run = false;
+            }
+            /*if(fileName.Text.Contains("rss") == false && fileName.Text.Contains("xml") == false && fileName.Text.Contains(""))
+            {
+                errorProvider1.SetError(fileName, "Invalid Feed filename. Extension must be \"xml\" or \"rss\"!");
+                run = false;
+            }*/
+            if(run == true)
+            {
+                upload(sender, e);
+            }
+                
+                
+        }
+        private void upload(object sender, EventArgs e)
+        {
+            try
+            {
+                string strTMP = directoryPath.Text.Trim();
+                progressBar1.Value = 0;
+                progressBar1.Increment(15);
+                UploadConnection = new FTPConnect();
+                UploadConnection.setRemoteHost(serverTextBox.Text);
+                UploadConnection.setRemoteUser(usernameTextBox.Text);
+                UploadConnection.setRemotePass(passwordTextBox.Text);
+                UploadConnection.setRemotePort(Convert.ToInt32(portTextBox.Text));
+                UploadConnection.login();
+                if (directoryPath.Text != "" || strTMP != "/")
+                {
+                    UploadConnection.chdir(directoryPath.Text);
+                }
+                progressBar1.Increment(10);
+                UploadConnection.upload(strFileName);
+                progressBar1.Increment(25);
+                UploadConnection.close();
+            }
+            catch (Exception ex)
+            {
+                if (uploadTry < 11)
+                {
+                    uploadTry = uploadTry + 1;
+                    upload(sender, e);
+                }
+                else
+                {
+                    MessageBox.Show("Connection Failed! It is possible that the FTP server you specified is unable to handle more connections currently, in which case try again later. Also verify your username, password, connection port, and desired output directory.", ex.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1);
+                }
+            }
+            if (uploadTry < 11)
+            {
+                MessageBox.Show("Feed successfully uploaded!", "Upload Successful!", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                this.Dispose();
+            }
+            uploadTry = 1;
+            progressBar1.Value = 0;
+            
         }
     }
 }
