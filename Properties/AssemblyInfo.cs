4689b1f989968c5245c199700ffbfaf5832c63a7
wizardspro <wizardspro@4e566d00-7630-0410-a8d3-e317d81b8117>
2007-06-22 11:40:23 +0000

    More bug fixes. Updated version numbers!

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Released under the GPL(Gnu Public License)")]
 [assembly: AssemblyProduct("Feed Launch .NET")]
-[assembly: AssemblyCopyright("KH 2007")]
+[assembly: AssemblyCopyright("wizardspro")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
